69a4cf454b465cb7ffd7a5a63906d421484af017
grubi <grubian2@gmail.com>
2022-07-19 09:36:07 +0300

    beautify date showing

--- src/components/Expenseitem.js
+++ src/components/Expenseitem.js
@@ -3,10 +3,19 @@ import './ExpenseItem.css'
 
 function ExpenseItem({date, title, amount}) {
 
+    const month = date.toLocaleString('en-US', {month: 'long'});
+    const year = date.getFullYear();
+    const day = date.toLocaleString('en-US', {day: '2-digit'});
+
     return (
         <div className='expense-item'>
-            <div className='expense-item__description'>{date.toISOString()}</div>
-            <div>c
+            <div>
+                <div>{month}</div>
+                <div>{year}</div>
+                <div>{day}</div>
+            </div>
+            <div className='expense-item__description'></div>
+            <div>
                 <h2>{title}</h2>
                 <div className='expense-item__price'>${amount}</div>
             </div>
