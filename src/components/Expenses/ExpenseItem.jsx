f45058408272c9d8974596060639592b786ade6e
grubi <grubian2@gmail.com>
2022-07-19 14:39:40 +0300

    feat: update title through state

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -1,17 +1,31 @@
+import { useState } from "react";
+
 import Card from "../UI/Card.jsx";
 
 import ExpenseDate from "./ExpenseDate.jsx";
 
 import "./ExpenseItem.css";
 
-const ExpenseItem = ({ date, title, amount }) => (
-  <Card className="expense-item">
-    <ExpenseDate date={date} />
-    <div className="expense-item__description">
-      <h2>{title}</h2>
-      <div className="expense-item__price">${amount}</div>
-    </div>
-  </Card>
-);
+const ExpenseItem = ({ date, title, amount }) => {
+  const [newTitle, setNewTitle] = useState(title);
+
+  const clickHandler = () => {
+    setNewTitle("Updated");
+    console.log("Clicked");
+  };
+
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={date} />
+      <div className="expense-item__description">
+        <h2>{newTitle}</h2>
+        <div className="expense-item__price">${amount}</div>
+      </div>
+      <button onClick={clickHandler} type="button">
+        Change Title
+      </button>
+    </Card>
+  );
+};
 
 export default ExpenseItem;
