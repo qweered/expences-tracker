5034e03ed915e890b53ff53c33530e31e64151e1
grubi <grubian2@gmail.com>
2022-07-23 16:03:49 +0300

    refactor: use map and state

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -19,26 +19,13 @@ const Expenses = ({ expenses }) => {
         onchangeFilter={filterChangeHandler}
         selected={filteredYear}
       />
-      <ExpenseItem
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-        title={expenses[0].title}
-      />
-      <ExpenseItem
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-        title={expenses[1].title}
-      />
-      <ExpenseItem
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-        title={expenses[2].title}
-      />
-      <ExpenseItem
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-        title={expenses[3].title}
-      />
+      {expenses.map((expense) => (
+        <ExpenseItem
+          amount={expense.amount}
+          date={expense.date}
+          title={expense.title}
+        />
+      ))}
     </Card>
   );
 };
